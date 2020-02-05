b86dd91513340ebac85d34d4c74e33c649546edc
Thomas Ballinger <me@ballingt.com>
2020-02-04 20:07:08 -0800

    prettier

--- index.js
+++ index.js
@@ -1,73 +1,72 @@
-
 (function() {
-// Find deepest DOM ancestor that does not change on reexecute
-const findParentOutputDiv = (el) => {
+  // Find deepest DOM ancestor that does not change on reexecute
+  const findParentOutputDiv = el => {
     let candidate = el;
     while (candidate) {
-        candidate = candidate.parentElement
-        if (candidate.className === 'output') {
-            return candidate;
-        }
+      candidate = candidate.parentElement;
+      if (candidate.className === "output") {
+        return candidate;
+      }
     }
     throw Error("parent output div not found");
-}
+  };
 
-// Find shallowest DOM ancestor that is removed on delete
-const findParentCell = (el) => {
-        let candidate = el;
-        while (candidate) {
-            candidate = candidate.parentElement
-            if (candidate.classList.contains('cell')) {
-                return candidate;
-            }
-        }
-        throw Error("cell not found not found");
+  // Find shallowest DOM ancestor that is removed on delete
+  const findParentCell = el => {
+    let candidate = el;
+    while (candidate) {
+      candidate = candidate.parentElement;
+      if (candidate.classList.contains("cell")) {
+        return candidate;
+      }
     }
+    throw Error("cell not found not found");
+  };
 
-const onCellReexecuteOrDelete = (element, cb) => {
-    
+  const onCellReexecuteOrDelete = (element, cb) => {
     const outputDiv = findParentOutputDiv(element);
     const cellDiv = findParentCell(element);
     const notebookContainer = cellDiv.parentElement;
-    let onEither = () => { throw Error('callback not initialized yet'); }
+    let onEither = () => {
+      throw Error("callback not initialized yet");
+    };
 
     const reexecuteDetector = function(mutationsList, observer) {
-        for(let mutation of mutationsList) {
-            if (mutation.type === 'childList') {
-                onEither('cell reexecute');
-                return;
-            }
+      for (let mutation of mutationsList) {
+        if (mutation.type === "childList") {
+          onEither("cell reexecute");
+          return;
         }
+      }
     };
-    
+
     const cellDeleteDetector = function(mutationsList, observer) {
-        for (let mutation of mutationsList) {
-            if (mutation.type === 'childList') {
-                for (let node of mutation.removedNodes) {
-                    if (node === cellDiv) {
-                        onEither('cell delete')
-                        return;
-                    }
-                }
-
+      for (let mutation of mutationsList) {
+        if (mutation.type === "childList") {
+          for (let node of mutation.removedNodes) {
+            if (node === cellDiv) {
+              onEither("cell delete");
+              return;
             }
+          }
         }
+      }
     };
 
-    const outputMutationObserver =  new MutationObserver(reexecuteDetector);
+    const outputMutationObserver = new MutationObserver(reexecuteDetector);
     const notebookMutationObserver = new MutationObserver(cellDeleteDetector);
-    
-    onEither = (reason) => {
-        outputMutationObserver.disconnect();
-        notebookMutationObserver.disconnect();
-        cb(reason);
-    }
-    
+
+    onEither = reason => {
+      outputMutationObserver.disconnect();
+      notebookMutationObserver.disconnect();
+      cb(reason);
+    };
+
     outputMutationObserver.observe(outputDiv, { childList: true });
     notebookMutationObserver.observe(notebookContainer, { childList: true });
-}
+  };
 
-    if (typeof window.onCellReexecuteOrDelete === 'undefined') {
-        window.onCellReexecuteOrDelete = onCellReexecuteOrDelete;
-    }
+  if (typeof window.onCellReexecuteOrDelete === "undefined") {
+    window.onCellReexecuteOrDelete = onCellReexecuteOrDelete;
+  }
 })();
